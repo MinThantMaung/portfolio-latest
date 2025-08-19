1e6719ea1cc453d4d656675c21d833d046f4dbfb
Min Thant Maung <minthant180@gmail.com>
2025-08-19 17:03:23 +0900

    feat:add static text

---
+++ src/app/static/index.ts
@@ -0,0 +1,9 @@
+export const staticData = {
+  title:
+    "I’m a frontend developer specializing in React.js, Next.js, and TypeScript, passionate about creating clean, scalable, and user-focused applications. <br><br>Alongside frontend, I’m expanding into backend development with Node.js, aiming to grow as a well-rounded fullstack engineer. <br><br>Based in Japan, I’m driven by continuous learning and a commitment to delivering high-quality solutions.",
+  name: "Hello,I'm Min Thant Maung",
+  role: "I'm",
+  skill_description:
+    "I focus on building scalable, user-friendly, and performance-driven web applications with clean architecture and strong attention to detail in design and usability.",
+  download: "Download",
+};
