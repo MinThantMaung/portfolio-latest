2c1156fb8128979a2e78cce1180cdd23d8a2098d
Min Thant Maung <minthant180@gmail.com>
2025-08-19 17:03:31 +0900

    feat: add skill data

---
+++ src/app/static/skill.ts
@@ -0,0 +1,87 @@
+export const skills = [
+  {
+    id: "1",
+    name: "React",
+    url: "/languages/react.svg",
+  },
+  {
+    id: "2",
+    name: "NextJs",
+    url: "/languages/nextjs.svg",
+  },
+  {
+    id: "3",
+    name: "typescript",
+    url: "/languages/typescript.svg",
+  },
+  {
+    id: "4",
+    name: "tailwind",
+    url: "/languages/tailwindCss.svg",
+  },
+  {
+    id: "5",
+    name: "javascript",
+    url: "/languages/javascript.svg",
+  },
+  {
+    id: "6",
+    name: "Html",
+    url: "/languages/html.svg",
+  },
+  {
+    id: "7",
+    name: "Css",
+    url: "/languages/css.svg",
+  },
+  {
+    id: "8",
+    name: "NodeJs",
+    url: "/languages/nodejs.svg",
+  },
+  {
+    id: "9",
+    name: "Express",
+    url: "/languages/express.svg",
+  },
+  {
+    id: "10",
+    name: "java",
+    url: "/languages/java.svg",
+  },
+  {
+    id: "11",
+    name: "spring",
+    url: "/languages/spring.svg",
+  },
+  {
+    id: "12",
+    name: "git",
+    url: "/languages/git.svg",
+  },
+  {
+    id: "13",
+    name: "mysql",
+    url: "/languages/mysql.svg",
+  },
+  {
+    id: "14",
+    name: "postman",
+    url: "/languages/postman.svg",
+  },
+  {
+    id: "15",
+    name: "vercel",
+    url: "/languages/vercel.svg",
+  },
+  {
+    id: "16",
+    name: "vscode",
+    url: "/languages/vscode.svg",
+  },
+  {
+    id: "17",
+    name: "webstorm",
+    url: "/languages/webstorm.svg",
+  },
+];
