b57f9b54fda820e181a7c5cf5078da8acdd5090b
Min Thant Maung <minthant180@gmail.com>
2025-08-19 17:56:38 +0900

    feat:add more data

--- src/app/static/index.ts
+++ src/app/static/index.ts
@@ -1,9 +1,15 @@
 export const staticData = {
   title:
-    "I’m a frontend developer specializing in React.js, Next.js, and TypeScript, passionate about creating clean, scalable, and user-focused applications. <br><br>Alongside frontend, I’m expanding into backend development with Node.js, aiming to grow as a well-rounded fullstack engineer. <br><br>Based in Japan, I’m driven by continuous learning and a commitment to delivering high-quality solutions.",
+    "I’m a frontend developer specializing in React.js, Next.js, and TypeScript, passionate about creating clean, scalable, and user-focused applications.",
+  subtitle:
+    "Alongside frontend, I’m expanding into backend development with Node.js, aiming to grow as a well-rounded fullstack engineer.",
+  title_description:
+    "Based in Japan, I’m driven by continuous learning and a commitment to delivering high-quality solutions.",
   name: "Hello,I'm Min Thant Maung",
   role: "I'm",
   skill_description:
-    "I focus on building scalable, user-friendly, and performance-driven web applications with clean architecture and strong attention to detail in design and usability.",
-  download: "Download",
+    "I picked up my skills by actually building things and figuring out problems along the way. Most of what I know comes from hands-on experience—testing ideas, fixing mistakes, and improving with every project. It’s less about theory and more about what I’ve learned by doing.",
+  download: "Download CV",
+  experience_and_certification: "Experience & Certification",
+  exp_descrition: "My Career Journey",
 };
