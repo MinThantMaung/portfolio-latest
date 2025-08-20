2debe3ad5797d0898b1852cb257756d85002a077
Min Thant Mg <minthant180@gmail.com>
2025-08-21 02:09:17 +0900

    fix:text color

--- src/app/static/quotes.tsx
+++ src/app/static/quotes.tsx
@@ -10,7 +10,7 @@ export const Highlight = ({
   return (
     <span
       className={cn(
-        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+        "font-bold text-emerald-500 px-1 py-0.5",
         className
       )}
     >
@@ -27,8 +27,8 @@ export const quotesItem = [
     slug: "/quotes/cr7.jpg",
     content: (
       <p className="italic">
-        "Your love makes me strong, your <Highlight>hate</Highlight> makes me{" "}
-        <Highlight>unstoppable.</Highlight>"
+        “Your love makes me strong, your <Highlight>hate</Highlight> makes me
+        <Highlight>unstoppable.</Highlight>”
       </p>
     ),
   },
