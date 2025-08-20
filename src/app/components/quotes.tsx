98966fd7e28e0968ad2dfeaecda8f9ec6081bcc8
Min Thant Mg <minthant180@gmail.com>
2025-08-21 02:08:18 +0900

    fix:delete title and add textgenerate effect

--- src/app/components/quotes.tsx
+++ src/app/components/quotes.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { CardStack } from "../ui/card-stack";
 import { quotesItem } from "../static/quotes";
 import { staticData } from "../static";
+import { TextGenerateEffect } from "../ui/text-generate-effect";
 
 const Quotes = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row mx-4 md:mx-32">
-        <div className="md:w-1/2 flex flex-col justify-center items-start md:mx-10">
-          <h2 className="font-bold text-3xl">Quotes</h2>
-          <div className="mt-6">{staticData.quote_description}</div>
+      <div className="flex flex-col lg:flex-row mx-4 md:mx-32">
+        <div className="lg:w-1/2 flex flex-col justify-center items-start md:mx-10">
+          {/* <h2 className="font-bold text-3xl">Quotes</h2> */}
+          <TextGenerateEffect words={staticData.quote_description} />
+          {/* <div className="mt-6 text-xl font-bold">{staticData.quote_description}</div> */}
         </div>
-        <div className="h-[40rem] flex items-center justify-center md:w-1/2">
+        <div className="h-[40rem] flex items-center justify-center lg:w-1/2">
           <CardStack items={quotesItem} />
         </div>
       </div>
