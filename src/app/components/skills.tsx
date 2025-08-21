dca6a3e3a75a4197e2312fece45ccc43cf40eee7
Min Thant Maung <minthant180@gmail.com>
2025-08-21 17:52:48 +0900

    feat:implement skills text

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -9,8 +9,12 @@ export function Skills() {
   return (
     <>
       <div className="py-12 md:py-32">
-        <h2 className="ml-4 md:ml-32 font-bold text-3xl">SKILLS</h2>
-        <div className="mx-4 md:mx-32 mt-6">{staticData.skill_description}</div>
+        <h2 className="font-bold text-3xl ml-4 md:ml-0 md:text-center">
+          SKILLS
+        </h2>
+        <div className="mx-4 md:mx-24 md:text-center mt-6">
+          {staticData.skill_description}
+        </div>
         <div className="h-52 bg-white flex flex-col antialiased items-center justify-center relative overflow-hidden my-12 md:my-20">
           <InfiniteMovingCards items={skills} direction="left" speed="slow" />
         </div>
