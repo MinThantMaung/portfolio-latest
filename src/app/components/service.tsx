0db7669e5fb8b4111c3caaad8370b304e4795cbf
Min Thant Maung <minthant180@gmail.com>
2025-08-21 17:52:32 +0900

    feat:implement server component

---
+++ src/app/components/service.tsx
@@ -0,0 +1,21 @@
+import { staticData } from "../static";
+import { serviceContent } from "../static/service";
+import { StickyScroll } from "../ui/sticky-scroll-reveal";
+
+const Service = () => {
+  return (
+    <>
+      <h2 className="ml-4 md:ml-0 md:text-center md:pt-14 mt-4 font-bold text-3xl">
+        SERVICES
+      </h2>
+      <div className="mx-4 md:mx-24 md:text-center mt-6">
+        {staticData.service_description}
+      </div>
+      <div className="w-full flex justify-center items-center md:py-12 py-4">
+        <StickyScroll content={serviceContent} />
+      </div>
+    </>
+  );
+};
+
+export default Service;
