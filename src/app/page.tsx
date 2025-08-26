6cc881ad7934062f3385369213ca88d7cf531ef0
Min Thant Maung <minthant180@gmail.com>
2025-08-26 16:16:28 +0900

    fix:reorder component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./(hero)/page";
+import Certification from "./components/certification";
 import Changelog from "./components/changelog";
+import Project from "./components/project";
 import Quotes from "./components/quotes";
 import Service from "./components/service";
 import { Skills } from "./components/skills";
-import Timeline from "./components/timeline/timeline";
+import Timeline from "./components/timeline";
 
 export default function Home() {
   return (
@@ -12,9 +14,11 @@ export default function Home() {
         <Hero />
         <Skills />
         <Timeline />
+        <Certification />
+        <Project />
         <Service />
-        <Quotes />
         <Changelog />
+        <Quotes />
       </div>
     </>
   );
