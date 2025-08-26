f1d6dd50510e5c694fc12d46abf7b9698f42c3c7
Min Thant Maung <minthant180@gmail.com>
2025-08-26 16:18:05 +0900

    fix: delete unused usage

---
+++ src/app/components/timeline.tsx
@@ -0,0 +1,21 @@
+import { experienceItems } from "@/app/static/experience";
+import { staticData } from "../static";
+import { StickyScroll } from "@/app/ui/sticky-scroll-reveal";
+
+const Timeline = () => {
+  return (
+    <>
+      <h2 className="ml-4 md:ml-0 md:text-center font-bold text-3xl pt-2 md:pt-6">
+        {staticData.experience}
+      </h2>
+      <div className="mx-4 md:mx-24 md:text-center mt-6">
+        {staticData.exp_description}
+      </div>
+      <div className="w-full md:py-12 py-4">
+        <StickyScroll content={experienceItems} />
+      </div>
+    </>
+  );
+};
+
+export default Timeline;
