16d0133a2d822c9b3760d719c8bb3b5c66037dfe
Min Thant Maung <minthant180@gmail.com>
2025-08-26 16:17:35 +0900

    feat:implement service feature

--- src/app/components/service.tsx
+++ src/app/components/service.tsx
@@ -1,19 +1,19 @@
 import { staticData } from "../static";
 import { serviceContent } from "../static/service";
-import { StickyScroll } from "../ui/sticky-scroll-reveal";
+import { HoverEffect } from "../ui/card-hover-effect";
 
 const Service = () => {
   return (
     <>
-      {/* <h2 className="ml-4 md:ml-0 md:text-center md:pt-14 mt-4 font-bold text-3xl">
+      <h2 className="ml-4 md:ml-0 md:text-center md:pt-14 pt-4 font-bold text-3xl">
         SERVICES
       </h2>
       <div className="mx-4 md:mx-24 md:text-center mt-6">
         {staticData.service_description}
       </div>
-      <div className="w-full flex justify-center items-center md:py-12 py-4">
-        <StickyScroll content={serviceContent} />
-      </div> */}
+      <div className="max-w-5xl mx-auto px-8">
+        <HoverEffect items={serviceContent} />
+      </div>
     </>
   );
 };
