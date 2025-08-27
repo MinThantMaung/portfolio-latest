e0185c109b972ddafef989bb9b064c07f09737cf
Min Thant Maung <minthant180@gmail.com>
2025-08-27 17:32:13 +0900

    fix:code splitting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-import Certification from "./components/certification";
-import Changelog from "./components/changelog";
+// app/page.tsx  (Server Component by default)
 import Hero from "./components/hero";
-import Project from "./components/project";
-import Quotes from "./components/quotes";
-import Service from "./components/service";
 import { Skills } from "./components/skills";
-import Timeline from "./components/timeline";
+import SectionsClient from "./components/section-client";
 
 export default function Home() {
   return (
-    <>
-      <div className="bg-white text-black">
-        <Hero />
-        <Skills />
-        <Timeline />
-        <Certification />
-        <Project />
-        <Service />
-        <Changelog />
-        <Quotes />
-      </div>
-    </>
+    <div className="bg-white text-black">
+      <Hero />
+      <Skills />
+      <SectionsClient />
+    </div>
   );
 }
